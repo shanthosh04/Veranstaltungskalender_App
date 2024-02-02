e570b5c3f6d5fab43f992e919a47759877e5ba94
shanthosh04 <shanthosh1610@gmail.com>
2024-02-02 17:04:05 +0100

    implement login

---
+++ client/scripts/login.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.getElementById("loginForm");
+    const errorText = document.getElementById("error");
+
+    loginForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const formData = new FormData(loginForm);
+        const data = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (!response.ok) throw new Error('Fehler beim Senden der Anfrage');
+
+            const result = await response.json();
+            if (result) {
+                window.location.href = "./index.html";
+            } else {
+                errorText.innerText = "Login fehlgeschlagen!";
+            }
+        } catch (error) {
+            errorText.innerText = error.message;
+        }
+    });
+});
