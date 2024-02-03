2c9eaa4b8883fe453d4ec919aaf47f27518bc4c9
shanthosh04 <shanthosh1610@gmail.com>
2024-02-03 15:29:40 +0100

    add event site

---
+++ client/scripts/index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var addButton = document.getElementById('addEventButton');
+    addButton.addEventListener('click', function() {
+        window.location.href = './addevent.html';
+    });
+});
