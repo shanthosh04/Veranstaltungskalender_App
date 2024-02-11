0a8cff2c2d11fde0554c83e0427fa695f8c9dc22
shanthosh04 <shanthosh1610@gmail.com>
2024-02-11 11:52:06 +0100

    add login in cypress

--- cypress/e2e/1-getting-started/event.cy.js
+++ cypress/e2e/1-getting-started/event.cy.js
@@ -1,29 +1,37 @@
-
-
-describe("This is a Test", () => {
-    beforeEach(() => {
-      cy.clearAllCookies();
-      cy.clearAllLocalStorage();
-      cy.clearAllSessionStorage();
-      cy.visit("http://localhost:3000/");
-  
-
-      cy.contains('Register').click();
-  
-
-      cy.get('input[name="firstname"]').type('shanthosh');
-      cy.get('input[name="lastname"]').type('sivas');
-      cy.get('input[name="birthdate"]').type('2004-10-16');
-      cy.get('input[name="street"]').type('zieglerstrasse 67');
-      cy.get('input[name="zipcode"]').type('3098');
-      cy.get('input[name="city"]').type('koeniz');
-      cy.get('input[name="email"]').type('shan@gmail.com');
-      cy.get('input[name="password"]').type('cypress');
-  
-
-      cy.get('button[type="submit"]').click(); 
-
-      cy.contains('Registration Successful').should('be.visible');
+describe('Event Operations', () => {
+    it('successfully loads the home page', () => {
+        cy.visit('http://localhost:3000/');
     });
-  });
-  
+});
+
+describe("User Registration", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/register");
+    });
+
+    it('allows a user to register', () => {
+        cy.get('input[name="firstname"]').type('shanthosh');
+        cy.get('input[name="lastname"]').type('sivas');
+        cy.get('input[name="birthdate"]').type('2004-10-16');
+        cy.get('input[name="street"]').type('zieglerstrasse 67');
+        cy.get('input[name="zipcode"]').type('3098');
+        cy.get('input[name="city"]').type('koeniz');
+        cy.get('input[name="email"]').type('shan@gmail.com');
+        cy.get('input[name="password"]').type('cypress');
+        cy.get('button[type="submit"]').click();
+
+        cy.contains('Registration Successful').should('be.visible');
+    });
+});
+
+describe('Login Operations', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/login');
+    });
+
+    it('allows a user to login', () => {
+        cy.get('input[name="email"]').type('shan@gmail.com');
+        cy.get('input[name="password"]').type('cypress');
+        cy.get('button[type="submit"]').click();
+    });
+});
